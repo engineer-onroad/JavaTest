9b47f8f315ff1cfae99613556d894ee370f9e948
engineer-onroad <qhdwd_bj@163.com>
2018-01-18 17:28:03 +0800

    日志配置

--- src/main/java/com/java/javassist/test/HelloTest.java
+++ src/main/java/com/java/javassist/test/HelloTest.java
@@ -3,12 +3,15 @@ package com.java.javassist.test;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by wudi06 on 2018/1/11.
  */
 public class HelloTest {
-    //private static  final Logger logger= LoggerFactory.getLogger(HelloTest.class);
+	
+    private static  final Logger logger= LoggerFactory.getLogger(HelloTest.class);
 
     public static void main(String[] args) throws Exception {
         ClassPool pool = ClassPool.getDefault();
@@ -20,6 +23,6 @@ public class HelloTest {
         Class c = cc.toClass();
         Hello h = (Hello)c.newInstance();
         h.say();
-        //logger.info("it's end");
+        logger.info("it's end");
     }
 }
